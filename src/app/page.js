bb936f9405dc1c7acf5a663030d2708a758a1fa0
Huraira-js <aboo.huraira.js@tafsol.com>
2024-09-13 22:27:58 +0500

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,160 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import React, { useState, useEffect } from "react";
+import { Container, Table } from "react-bootstrap";
+import classes from "./page.module.css";
+import moment from "moment";
+import ReactSelect from "react-select";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useDebounce } from "../components/UseDebounce/useDebounce";
 
-export default function Home() {
+const Home = () => {
+  const [faqs, setFaqs] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [statusFilter, setStatusFilter] = useState("all");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const debouncedSearch = useDebounce(searchTerm);
+
+  const options = [
+    { value: "all", label: "All statuses" },
+    { value: "true", label: "Active" },
+    { value: "false", label: "Inactive" },
+  ];
+
+  async function fetchData(
+    page = currentPage,
+    search = debouncedSearch,
+    filter = statusFilter
+  ) {
+    setLoading(true);
+    try {
+      const url = "https://7l60bxbl-3093.inc1.devtunnels.ms/api/v1/faqs";
+      const response = await fetch(
+        `${url}?limit=10&page=${page}&search=${search}&status=${filter}`,
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2ZDY0NTc0NTUyNDJmYjYwNjQ5MGY0MCIsImlhdCI6MTcyNjA3NTQ5OCwiZXhwIjoxNzI2MTYxODk4fQ.OJxFgnHU95ZhOVTBPUM3ghBv2xDHSsNJW-MKo91AmkM",
+            "ngrok-skip-browser-warning": "69420",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      setFaqs(data.data.faqs);
+      setTotalPages(Math.ceil(data.data.totalCount / 10));
+    } catch (err) {
+      console.error("Fetch error:", err);
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData(1, debouncedSearch, statusFilter);
+    setCurrentPage(1);
+  }, [debouncedSearch]);
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleStatusChange = (selectedOption) => {
+    const value = selectedOption ? selectedOption.value : "all";
+    setStatusFilter(value);
+    setCurrentPage(1);
+    fetchData(1, debouncedSearch, value);
+  };
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    fetchData(page, searchTerm, statusFilter);
+  };
+
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <Container className={classes.mainBody}>
+      <div className={classes.contentBody}>
+        <h1 className={classes.mainHeading}>FAQs</h1>
+        <div className={classes.inputs}>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={handleSearchChange}
+            placeholder="Search FAQs"
+            className={classes.searchInput}
+          />
+          <ReactSelect
+            options={options}
+            placeholder="Select status"
+            value={options.find((option) => option.value === statusFilter)}
+            onChange={handleStatusChange}
+            className={classes.statusFilter}
+          />
+        </div>
+
+        {loading ? (
+          <h1 className={classes.loading}>Loading...</h1>
+        ) : error ? (
+          <p>Error: {error.message}</p>
+        ) : faqs.length > 0 ? (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Question</th>
+                <th>Answer</th>
+                <th>Category</th>
+                <th>Active</th>
+                <th>Created At</th>
+                <th>Updated At</th>
+              </tr>
+            </thead>
+            <tbody>
+              {faqs.map((faq, index) => (
+                <tr key={faq._id}>
+                  <td>{(currentPage - 1) * 10 + index + 1}</td>
+                  <td>{faq.question}</td>
+                  <td>{faq.answer}</td>
+                  <td>{faq.category.name}</td>
+                  <td>{faq.active ? "Yes" : "No"}</td>
+                  <td>{moment().format("MMM Do YY")}</td>
+                  <td>{moment(faq.updatedAt).format("MMM Do YY")}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        ) : (
+          <h1 className={classes.loading}>No FAQs available.</h1>
+        )}
+
+        <div className={classes.paginationControls}>
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              key={index + 1}
+              onClick={() => handlePageChange(index + 1)}
+              className={
+                currentPage === index + 1
+                  ? classes.activePageButton
+                  : classes.pageButton
+              }
+            >
+              {index + 1}
+            </button>
+          ))}
         </div>
       </div>
+    </Container>
+  );
+};
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
